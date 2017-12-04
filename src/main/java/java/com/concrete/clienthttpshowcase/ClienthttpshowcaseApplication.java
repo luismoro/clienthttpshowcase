20f8d44416dde8d6f7ee3e7ec9cf908aa9837033
Luis Moro <luis.moro@concrete.com.br>
2017-12-04 09:31:14 -0200

    Commit Inicial

---
+++ src/main/java/java/com/concrete/clienthttpshowcase/ClienthttpshowcaseApplication.java
@@ -0,0 +1,12 @@
+package java.com.concrete.clienthttpshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienthttpshowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienthttpshowcaseApplication.class, args);
+	}
+}
